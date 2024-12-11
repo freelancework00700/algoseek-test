7835e4b3b60851c9920b56050fd50a85cf68d182
freelancework00700 <freelancework00700@gmail.com>
2024-12-11 16:15:57 +0530

    task-1: added tooltip and confirmation modal

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,11 @@
 import { routes } from './app.routes';
 import { provideRouter } from '@angular/router';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)],
+  providers: [
+    provideRouter(routes), provideAnimationsAsync(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+  ],
 };
--- src/app/task-1/task-1.component.ts
+++ src/app/task-1/task-1.component.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
+import { NgxDialog } from '@ngx-popovers/dialog';
 import { Component, OnInit } from '@angular/core';
+import { NgxTooltip } from '@ngx-popovers/tooltip';
 import { RelativeTimePipe } from '../pipes/relative-time.pipe';
 import { FormGroup, Validators, FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
@@ -14,39 +16,45 @@ interface Note {
   selector: 'app-task-1',
   styleUrl: './task-1.component.scss',
   templateUrl: './task-1.component.html',
-  imports: [FormsModule, CommonModule, ReactiveFormsModule, RelativeTimePipe],
+  imports: [
+    NgxDialog,
+    NgxTooltip,
+    FormsModule,
+    CommonModule,
+    RelativeTimePipe,
+    ReactiveFormsModule,
+  ],
 })
 export class Task1Component implements OnInit {
   noteForm!: FormGroup;
-  currentUser: string = 'You';
   formSubmitted: boolean = false;
   selectedNoteType: string = 'Message';
-  userName: string = 'Milton Romaguera';
+  userName: string = this.generateRandomName();
   noteTypes: string[] = ['Message', 'Phone', 'Coffee', 'Beer', 'Meeting'];
 
   notes: Note[] = [
     {
       type: 'Meeting',
-      user: this.currentUser,
+      user: this.userName,
       content: 'A more formal meeting.',
       timestamp: new Date(Date.now() - 3 * 24 * 60 * 60 * 1000), // 3 days ago
     },
     {
       content: 'Then we had a follow-up phone call.',
       type: 'Phone',
-      user: this.currentUser,
+      user: this.userName,
       timestamp: new Date(Date.now() - 5 * 24 * 60 * 60 * 1000), // 5 days ago
     },
     {
       content: 'We had coffee!',
       type: 'Coffee',
-      user: this.currentUser,
+      user: this.userName,
       timestamp: new Date(Date.now() - 6 * 24 * 60 * 60 * 1000), // 6 days ago
     },
     {
       content: 'A test note of message type!',
       type: 'Message',
-      user: this.currentUser,
+      user: this.userName,
       timestamp: new Date(Date.now() - 14 * 24 * 60 * 60 * 1000), // 14 days ago
     },
   ];
@@ -71,7 +79,7 @@ export class Task1Component implements OnInit {
       this.notes.unshift({
         content: this.noteForm.get('content')?.value,
         type: this.noteForm.get('type')?.value || 'Message',
-        user: this.currentUser,
+        user: this.userName,
         timestamp: new Date(),
       });
       this.noteForm.reset({ type: 'Message', content: '' });
@@ -93,5 +101,40 @@ export class Task1Component implements OnInit {
 
   deleteNote(index: number) {
     this.notes.splice(index, 1);
+  }
+
+  generateRandomName() {
+    const firstNames = [
+      'John',
+      'Emma',
+      'James',
+      'Olivia',
+      'Michael',
+      'Sophia',
+      'William',
+      'Isabella',
+      'Alexander',
+      'Mia',
+      'David',
+      'Charlotte',
+    ];
+
+    const lastNames = [
+      'Smith',
+      'Johnson',
+      'Brown',
+      'Williams',
+      'Jones',
+      'Garcia',
+      'Miller',
+      'Davis',
+      'Martinez',
+      'Hernandez',
+    ];
+
+    const randomFirstName = firstNames[Math.floor(Math.random() * firstNames.length)];
+    const randomLastName = lastNames[Math.floor(Math.random() * lastNames.length)];
+
+    return `${randomFirstName} ${randomLastName}`;
   }
 }
