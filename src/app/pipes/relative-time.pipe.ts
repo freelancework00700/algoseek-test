9b9119fe71397bad1e043733af9ef9b4fb87e012
freelancework00700 <freelancework00700@gmail.com>
2024-12-12 09:59:54 +0530

    task-3 completed

--- src/app/pipes/relative-time.pipe.ts
+++ src/app/pipes/relative-time.pipe.ts
@@ -11,13 +11,13 @@ export class RelativeTimePipe implements PipeTransform {
       return 'now';
     } else if (diffInSeconds < 3600) {
       const minutes = Math.floor(diffInSeconds / 60);
-      return `${minutes} m`;
+      return `${minutes}m`;
     } else if (diffInSeconds < 86400) {
       const hours = Math.floor(diffInSeconds / 3600);
-      return `${hours} h`;
+      return `${hours}h`;
     } else {
       const days = Math.floor(diffInSeconds / 86400);
-      return `${days} d`;
+      return `${days}d`;
     }
   }
 }
--- src/app/task-3/task-3.component.ts
+++ src/app/task-3/task-3.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import Swiper from 'swiper';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-task-3',
   imports: [],
+  selector: 'app-task-3',
+  styleUrl: './task-3.component.scss',
   templateUrl: './task-3.component.html',
-  styleUrl: './task-3.component.scss'
 })
-export class Task3Component {
+export class Task3Component implements OnInit {
+  swiper!: Swiper;
 
+  ngOnInit() {
+    this.swiper = new Swiper('.swiper', {});
+  }
 }
