72b639d8eb1c468a226ac43919a41e020b1c0735
hardik <hrsanghani157@gmail.com>
2024-12-12 09:22:16 +0530

    -task-2 UI

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { Task1Component } from './task-1/task-1.component';
 import { Task2Component } from './task-2/task-2.component';
+import { Task3Component } from './task-3/task-3.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'task-1', component: Task1Component },
   { path: 'task-2', component: Task2Component },
+  { path: 'task-3', component: Task3Component },
   { path: '', redirectTo: '/home', pathMatch: 'full' }, // default route
   { path: '**', redirectTo: '/home' }, // wildcard route (optional)
 ];
---
+++ src/app/task-3/task-3.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-3',
+  imports: [],
+  templateUrl: './task-3.component.html',
+  styleUrl: './task-3.component.scss'
+})
+export class Task3Component {
+
+}
